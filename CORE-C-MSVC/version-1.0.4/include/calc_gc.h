475e16eb72b697cc84d722b588220f6aea89c010
LoserOne-ovo <77661408+LoserOne-ovo@users.noreply.github.com>
2021-09-08 16:46:23 +0800

    Delete CORE-C-MSVC/version-1.0.4 directory

--- CORE-C-MSVC/version-1.0.4/include/calc_gc.h
+++
@@ -1,8 +0,0 @@
-#ifndef _CALC_GC_H
-#define _CALC_GC_H
-
-
-unsigned long long* _calc_geometry_center(unsigned int* label_res, unsigned int label_num, int rows, int cols);
-
-#endif
-
--- CORE-C-MSVC/version-1.0.4/include/get_reverse_fdir.h
+++
@@ -1,6 +0,0 @@
-#ifndef _GET_REVERSE_FDIR_H_
-#define _GET_REVERSE_FDIR_H_
-
-unsigned char* _get_re_dir(unsigned char* fdir, int rows, int cols);
-
-#endif // !_GET_REVERSE_FDIR_H_
